f3f1f030d8e4851fe64ab0f56e422555c1f2511d
Nikhil M S <nikhil.sreenivasa@costrategix.com>
2022-06-14 20:09:23 +0530

    Added comments.

--- src/components/area/api.js
+++ src/components/area/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const { REACT_APP_API_URL } = process.env;
 
+// Get Autocomplete.
 export async function getAutocomplete(value) {
   try {
     const { data } = await axios.get(
@@ -12,6 +13,7 @@ export async function getAutocomplete(value) {
   }
 }
 
+// Get location details.
 export async function getpostalCode(value) {
   try {
     const { data } = await axios.get(`${REACT_APP_API_URL}postcodes/${value}`);
